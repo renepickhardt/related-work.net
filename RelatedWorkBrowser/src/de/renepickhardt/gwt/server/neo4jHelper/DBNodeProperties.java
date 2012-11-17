070349187230d63a5e9c14d44824c1530df70aa3
Rene Pickhardt <rene@rene-pickhardt.de>
2012-11-18 00:18:13 +0800

    cleaned up

--- RelatedWorkBrowser/src/de/renepickhardt/gwt/server/neo4jHelper/DBNodeProperties.java
+++
@@ -1,7 +0,0 @@
-package de.renepickhardt.gwt.server.neo4jHelper;
-
-public class DBNodeProperties {
-	public static String PAPER_TITLE = "title";
-	public static String PAPER_CITATION_COUNT = "rw:citationCount";
-	public static String PAGE_RANK_VALUE = "pageRankValue";
-}
