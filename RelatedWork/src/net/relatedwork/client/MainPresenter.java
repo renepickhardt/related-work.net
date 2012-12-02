ba779cd4fc9202ddcf30769c4a840cf6dd0d2845
Heinrich <derhein@gmail.com>
2012-12-02 20:22:38 +0000

    Moved Discussion Presenter to about page

--- RelatedWork/src/net/relatedwork/client/MainPresenter.java
+++ RelatedWork/src/net/relatedwork/client/MainPresenter.java
@@ -36,9 +36,6 @@ public class MainPresenter extends
 	public static final Type<RevealContentHandler<?>> TYPE_SetMainContent = new Type<RevealContentHandler<?>>();
 
 	@ContentSlot
-	public static final Type<RevealContentHandler<?>> TYPE_Discussion = new Type<RevealContentHandler<?>>();
-
-	@ContentSlot
 	public static final Type<RevealContentHandler<?>> TYPE_Footer = new Type<RevealContentHandler<?>>();
 
 	@ContentSlot
@@ -74,7 +71,6 @@ public class MainPresenter extends
 
 	@Inject FooterPresenter footerPresenter;
 	@Inject HomePresenter homePresenter;
-	@Inject CommentPresenter commentPresenter;
 	@Inject HeaderPresenter headerPresenter;
 	@Inject BreadcrumbsPresenter breadcrumbsPresenter;
 
@@ -92,7 +88,6 @@ public class MainPresenter extends
 	protected void onReveal() {
 		super.onReveal();
 		setInSlot(TYPE_Footer, footerPresenter);
-		setInSlot(TYPE_Discussion, commentPresenter);
 		setInSlot(TYPE_Header, headerPresenter);
 		
 		// Register Session
