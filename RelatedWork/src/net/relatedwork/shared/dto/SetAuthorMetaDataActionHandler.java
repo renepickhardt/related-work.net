2bffccd7d2f8e7d334943865da06a9559428fa47
Rene Pickhardt <rene@rene-pickhardt.de>
2012-12-30 21:22:32 +0800

    forgot to add files when creating the sidebar

---
+++ RelatedWork/src/net/relatedwork/shared/dto/SetAuthorMetaDataActionHandler.java
@@ -0,0 +1,24 @@
+package net.relatedwork.shared.dto;
+
+import com.gwtplatform.dispatch.shared.UnsecuredActionImpl;
+import net.relatedwork.shared.dto.SetAuthorMetaDataActionHandlerResult;
+import java.util.HashMap;
+
+public class SetAuthorMetaDataActionHandler extends
+		UnsecuredActionImpl<SetAuthorMetaDataActionHandlerResult> {
+
+	private HashMap<String, String> metaData;
+
+	@SuppressWarnings("unused")
+	private SetAuthorMetaDataActionHandler() {
+		// For serialization only
+	}
+
+	public SetAuthorMetaDataActionHandler(HashMap<String, String> metaData) {
+		this.metaData = metaData;
+	}
+
+	public HashMap<String, String> getMetaData() {
+		return metaData;
+	}
+}
